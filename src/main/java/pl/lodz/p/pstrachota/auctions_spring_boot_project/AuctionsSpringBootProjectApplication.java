a2a88a8525d2b7e6c1d4d8eae6855b25f29d0e21
pStrachota <p.strachota52@gmail.com>
2022-08-26 19:28:07 +0200

    Initial commit

---
+++ src/main/java/pl/lodz/p/pstrachota/auctions_spring_boot_project/AuctionsSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.pstrachota.auctions_spring_boot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionsSpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionsSpringBootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lodz/p/pstrachota/auctions_spring_boot_project/AuctionsSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.pstrachota.auctions_spring_boot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionsSpringBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
